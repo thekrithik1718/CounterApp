33a19319e78a561e7a253fd08bcc56c1d82a0d78
thekrithik1718 <thekrithik1718@gmail.com>
2026-03-07 20:27:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const increasebtn=document.getElementById('increasebtn');
+const decreasebtn=document.getElementById('decreasebtn');
+const resetbtn=document.getElementById('resetbtn');
+const countlabel=document.getElementById('countlabel')
+
+let count=0;
+
+increasebtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+decreasebtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+resetbtn.onclick = function(){
+    count=0;
+    countlabel.textContent= count;
+}
